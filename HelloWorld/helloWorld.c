c41de03234665d2406f6f9470a90f09c342afdfb
Ajays1210 <ajaysgr1210@gmail.com>
2026-01-21 00:31:33 +0530

    Basics

---
+++ HelloWorld/helloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World\n");
+
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Your number is: %d\n", num);
+
+    system("pause");
+    return 0;
+
+}
---
+++ PrintName/printName.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, My name is Ajay S");
+    return 0;
+}
